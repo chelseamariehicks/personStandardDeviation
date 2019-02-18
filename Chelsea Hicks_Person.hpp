6c1d1f0588a1900021b5f8905206e4e6ccf91711
Chelsea Marie Hicks <chelseamariehicks@gmail.com>
2019-02-18 19:01:31 +0200

    Update Chelsea Hicks_Person.hpp

--- Chelsea Hicks_Person.hpp
+++ Chelsea Hicks_Person.hpp
@@ -1,8 +1,8 @@
 /***********************************************************************
-** Author:			Chelsea Hicks
-** Date:			10/31/2018
+** Author:		Chelsea Hicks
+** Date:		10/31/2018
 ** Description:		This is the class specification file for the 
-**					implementation file Person.cpp.
+**			implementation file Person.cpp.
 ***********************************************************************/
 
 #ifndef PERSON_HPP
